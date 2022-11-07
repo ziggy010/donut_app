506e000df253e0d303bf84511ee883ede095c88f
ARETEE <tajale01@gmail.com>
2022-11-07 10:24:43 +0545

    init

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:donut_app/views/homepage.dart';
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
